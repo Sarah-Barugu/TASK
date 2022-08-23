99dce884ff0516d1d492e7f994ff502e0161cad6
Sarah-Barugu <sarahtokoni23@gmail.com>
2022-08-23 17:55:01 +0100

    fifth commit- alert the largest number in the array

--- task2/index.js
+++ task2/index.js
@@ -1,1 +1,14 @@
-console.log(6);
+// created a variable with array values 
+let arr  = [2, 5, 3, -4, 8]
+// created a variable largest  
+let largest = arr[0];
+// alert the largest number in the array
+for(let index = 0; index < arr.length; index++) {
+  if (arr[index] > largest) {
+    largest = arr[index] 
+  }
+
+}
+
+alert(largest)
+

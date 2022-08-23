d1fda61dbdb302d802bd600dedb2b9e970ce25a1
Sarah-Barugu <sarahtokoni23@gmail.com>
2022-08-23 17:01:12 +0100

    third commit- created an if statement in task1 js file

--- task1/lndex.js
+++ task1/lndex.js
@@ -1,6 +1,11 @@
 'use strict'
 // create a for loop that iterates from 1 to 15
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
+for (let index = 1; index < 15; index++) {
+// create an if statement
+  if (index % 2 === 1) {
+    const h1 = document.createElement("h1")
+    h1.innerHTML = `${index} is odd` 
+    document.body.appendChild("h1")
+  } 
   
 }

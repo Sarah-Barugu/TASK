053ecd1ec0e9d3bcda644f8dbcfa5fb508243b37
Sarah-Barugu <sarahtokoni23@gmail.com>
2022-08-23 17:29:27 +0100

    fourthcommit- created an else statement

---
+++ task1/index.js
@@ -0,0 +1,16 @@
+// create a for loop that iterates from 1 to 15
+console.log(22);
+for (let index = 1; index <= 15; index++) {
+// create an if statement
+  if (index % 2 === 1) {
+    const h1 = document.createElement("h1")
+    h1.innerHTML = `${index} is odd` 
+    document.body.appendChild(h1)
+  } //create an else statement
+  else{
+    const h1 = document.createElement("h1")
+    h1.innerHTML = `${index} is even`
+    document.body.appendChild(h1)
+  }
+  
+}
